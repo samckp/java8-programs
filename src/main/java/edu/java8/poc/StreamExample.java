8d5aad3b0cddbf751cf93fa5eac171065f186cb7
samckp <sam.mca.liet@gmail.com>
2020-09-18 08:58:35 +0530

    updated stream example

--- src/main/java/edu/java8/poc/StreamExample.java
+++ src/main/java/edu/java8/poc/StreamExample.java
@@ -1,5 +1,6 @@
 package edu.java8.poc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -20,5 +21,23 @@ public class StreamExample {
         List<String> list = Arrays.asList("nodejs", "java", "python", "c++");
         list.forEach(System.out::println);
 
+        System.out.println("Create Stream example!");
+        Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9);
+        stream.forEach(p -> System.out.println(p));
+
+        System.out.println("Create Stream array example!");
+        Stream<Integer> streamArray = Stream.of( new Integer[]{1,2,3,4,5,6,7,8,9} );
+        streamArray.forEach(p -> System.out.println(p));
+
+
+        System.out.println("Iterate Stream List example!");
+        List<Integer> lists = new ArrayList<>();
+
+        for(int i = 1; i< 10; i++){
+            lists.add(i);
+        }
+
+        Stream<Integer> streamList = lists.stream();
+        streamList.forEach(p -> System.out.println(p));
     }
 }
