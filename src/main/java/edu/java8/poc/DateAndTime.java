a14200a80d3e11b48dbdd097021fdb180f2fba98
samckp <sam.mca.liet@gmail.com>
2021-04-04 07:37:31 +0530

    date time examples added

--- src/main/java/edu/java8/poc/DateAndTime.java
+++ src/main/java/edu/java8/poc/DateAndTime.java
@@ -1,9 +1,6 @@
 package edu.java8.poc;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 
 public class DateAndTime {
 
@@ -32,5 +29,36 @@ public class DateAndTime {
         System.out.println( datetime );
         System.out.println( datetimeFromClock );
 
+        dataTime(); // different examples of datetimeLocal
+    }
+
+    public static void dataTime(){
+
+        LocalDateTime dateTime = LocalDateTime.now();//returns the system date time in the default timezone.
+        System.out.println("dateTime from the system is:"+dateTime);
+
+//creates a date time with the values provided as parameters in the sequence year, month, day of the month, hour of the day, minute of the hour. The second and nano second are set to 0.
+        LocalDateTime ldtm = LocalDateTime.of(2014, 3, 30, 12, 30);
+        System.out.println("dateTime created by passing in year, month,day, hour, minute is:"+ldtm.toString());
+
+//creates a date time with the values provided as parameters in the sequence year, month, day of the month, hour of the day, minute of the hour, seconds of the minute. The nano second is set to 0.
+        LocalDateTime ldts = LocalDateTime.of(2014, 3, 30, 12, 30,23);
+        System.out.println("dateTime created by passing in year, month,day, hour, minute, seconds is :"+ldts.toString());
+
+//creates a date time with the values provided as parameters in the sequence year, month, day of the month, hour of the day, minute of the hour, seconds of the minute and nano second of the second.
+        LocalDateTime ldtns = LocalDateTime.of(2014, 3, 30, 12, 30,23,12000);
+        System.out.println("dateTime created by passing in year, month,day, hour, minute, seconds, nano seconds is :"+ldtns.toString());
+
+//creates a date time with the values provided as parameters in the sequence year, month value from the Month enum, day of the month, hour of the day, minute of the hour. The second and nano second are set to 0.
+        LocalDateTime ldtem = LocalDateTime.of(2013, Month.APRIL, 1, 12, 30);
+        System.out.println("The date time object created by passing in year, month from enum, day, hour , minute is : "+ldtem.toString());
+
+//creates a date time with the values provided as parameters in the sequence year, month value from the Month enum, day of the month, hour of the day, minute of the hour, seconds of the minute. The nano second is set to 0.
+        LocalDateTime ldtes = LocalDateTime.of(2013, Month.APRIL, 1, 12, 30,42);
+        System.out.println("The date time object created by passing in year, month from enum, day, hour , minute,second is : "+ldtes.toString());
+
+//creates a date time with the values provided as parameters in the sequence year, month value from the Month enum, day of the month, hour of the day, minute of the hour, seconds of the minute and nano seconds of the seconds.
+        LocalDateTime ldtens = LocalDateTime.of(2013, Month.APRIL, 1, 12, 30,42,12000);
+        System.out.println("The date time object created by passing in year, month from enum, day, hour , minute,seconds, ns is : "+ldtens.toString());
     }
 }
