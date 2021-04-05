a0f2c730b8bad134654a8e4ed716a839dcfa045e
samckp <sam.mca.liet@gmail.com>
2021-04-05 09:39:27 +0530

    date time methods added

--- src/main/java/edu/java8/poc/DateAndTime.java
+++ src/main/java/edu/java8/poc/DateAndTime.java
@@ -30,6 +30,7 @@ public class DateAndTime {
         System.out.println( datetimeFromClock );
 
         dataTime(); // different examples of datetimeLocal
+        datetimeMethods();
     }
 
     public static void dataTime(){
@@ -60,5 +61,28 @@ public class DateAndTime {
 //creates a date time with the values provided as parameters in the sequence year, month value from the Month enum, day of the month, hour of the day, minute of the hour, seconds of the minute and nano seconds of the seconds.
         LocalDateTime ldtens = LocalDateTime.of(2013, Month.APRIL, 1, 12, 30,42,12000);
         System.out.println("The date time object created by passing in year, month from enum, day, hour , minute,seconds, ns is : "+ldtens.toString());
+    }
+
+    public static void  datetimeMethods(){
+
+        final LocalDateTime dateTime = LocalDateTime.now();
+        int dayOfMonth = dateTime.getDayOfMonth();//returns the day of the month.
+        System.out.println("day Of Month is :"+dayOfMonth);
+        DayOfWeek dow = dateTime.getDayOfWeek();//returns DayOfWeek enum.
+        System.out.println("dow:"+dow);
+        int dayOfYear = dateTime.getDayOfYear();//returns the day of the year.
+        System.out.println("day Of Year is :"+dayOfYear);
+        int hour = dateTime.getHour();//returns hour of the day.
+        System.out.println("hour:"+hour);
+        int monthValue = dateTime.getMonthValue();//returns the number of the month in the year.
+        System.out.println("month of the date in number is:"+monthValue);
+        Month month = dateTime.getMonth();// returns the month enum for the month of the year.
+        System.out.println("month of the date is :"+month);
+        int sec = dateTime.getSecond();// returns seconds field value for the date time.
+        System.out.println("Seconds of the date time is:"+sec);
+        int nano = dateTime.getNano(); // returns nano seconds field value for the date time.
+        System.out.println("nano seconds of the date time is:"+nano);
+        int year = dateTime.getYear(); // returns year field value for the date time.
+        System.out.println("year of the date is :"+year);
     }
 }
