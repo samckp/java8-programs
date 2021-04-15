c22774ec6347e4f15c7e31e74bcc47d82945ff26
samckp <sam.mca.liet@gmail.com>
2021-04-15 09:53:08 +0530

    updated

--- src/main/java/edu/java8/poc/FilterExample.java
+++ src/main/java/edu/java8/poc/FilterExample.java
@@ -19,6 +19,18 @@ public class FilterExample {
 
     public static void main(String[] args) {
 
+        List<Product> productsList = initProduct();
+        productsList.stream()
+                .filter(p -> p.price < 30000)
+                .map(pm -> pm.price)
+                .forEach(System.out::println);
+
+        // fetching filtered data as a list
+        
+    }
+
+    public static List<Product> initProduct(){
+
         List<Product> productsList = new ArrayList<Product>();
         //Adding Products
         productsList.add(new Product(1,"HP Laptop",25000f));
@@ -26,10 +38,6 @@ public class FilterExample {
         productsList.add(new Product(3,"Lenevo Laptop",28000f));
         productsList.add(new Product(4,"Sony Laptop",28000f));
         productsList.add(new Product(5,"Apple Laptop",90000f));
-
-        productsList.stream()
-                .filter(p -> p.price < 30000)
-                .map(pm -> pm.price)
-                .forEach(System.out::println);
+        return productsList;
     }
 }
