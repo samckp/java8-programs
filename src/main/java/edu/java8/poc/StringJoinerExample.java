0734c08015f0cc5ba02ce2183eae3ca823e5e7b0
samckp <sam.mca.liet@gmail.com>
2021-05-01 12:15:46 +0530

    string joiner prog

---
+++ src/main/java/edu/java8/poc/StringJoinerExample.java
@@ -0,0 +1,18 @@
+package edu.java8.poc;
+
+import java.util.StringJoiner;
+
+public class StringJoinerExample {
+
+    public static void main(String[] args) {
+        StringJoiner joinNames = new StringJoiner(","); // passing comma(,) as delimiter
+
+        // Adding values to StringJoiner
+        joinNames.add("Rahul");
+        joinNames.add("Raju");
+        joinNames.add("Peter");
+        joinNames.add("Raheem");
+
+        System.out.println(joinNames);
+    }
+}
